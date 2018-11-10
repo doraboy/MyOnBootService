45b8d03d236b65a22d5b52278fcc925a417a350b
kleinerQ <chih.yen5408@gmail.com>
2018-11-10 10:25:08 +0800

    v2

--- app/src/main/java/tw/dora/myonbootservice/MainActivity.java
+++ app/src/main/java/tw/dora/myonbootservice/MainActivity.java
@@ -2,6 +2,10 @@ package tw.dora.myonbootservice;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +14,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
